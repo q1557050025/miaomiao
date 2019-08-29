b1a8df39bca0b4f0a0cba8946db21df8a039bda5
Buke <43104602+q1557050025@users.noreply.github.com>
2019-08-29 16:58:39 +0800

    add

---
+++ src/views/Cinema/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Header title="喵喵影院"></Header>
+    <tab-bar></tab-bar>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header";
+import tabBar from "@/components/TabBar"; 
+export default {
+  name: "Cinema",
+  components: {
+    Header,
+    tabBar
+  }
+}
+</script>
---
+++ src/views/Profile/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Header title="我的电影"></Header>
+    <tab-bar></tab-bar>
+  </div>
+</template>
+<script>
+  import Header from "@/components/Header";
+  import tabBar from "@/components/TabBar"; 
+  export default {
+    name: "Profile",
+    components: {
+      Header,
+      tabBar
+    }
+  }
+</script>
